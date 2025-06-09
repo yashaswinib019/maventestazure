56da4febf60b0f7d7360f700da1411de360ef6ee
yashaswinib019 <saiyashaswini.22cse@cambridge.edu.in>
2025-06-09 16:29:14 +0530

    Add files via upload

---
+++ src/main/java/gardle/tg/App.java
@@ -0,0 +1,13 @@
+package gardle.tg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello my World!" );
+    }
+}
